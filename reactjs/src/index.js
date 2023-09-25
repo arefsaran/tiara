a7efd00d8ec5962f15c71b34a5d6b76ed163a96a
arefsaran <34871233+arefsaran@users.noreply.github.com>
2023-09-25 12:01:11 +0330

    delete react files

--- reactjs/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const getMessage = (siteName, message) => {
-    return `${siteName} says: ${message}`;
-};
-ReactDOM.render(getMessage("aref.ir", "Hi"), document.getElementById("app"));
